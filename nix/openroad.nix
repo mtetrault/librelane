69593b49181eaf9e2a67b3a9edad667ace343e5b
mtetrault <52456402+mtetrault@users.noreply.github.com>
2026-02-13 12:04:11 -0500

    Try fix for failed fetches

--- nix/openroad.nix
+++ nix/openroad.nix
@@ -70,12 +70,13 @@ in
     pname = "openroad";
     version = rev-date;
 
-    src = fetchFromGitHub {
-      owner = "The-OpenROAD-Project";
-      repo = "OpenROAD";
-      inherit rev;
-      inherit sha256;
-    };
+    #src = fetchFromGitHub {
+    #  owner = "The-OpenROAD-Project";
+    #  repo = "OpenROAD";
+    #  inherit rev;
+    #  inherit sha256;
+    #};
+    src = ~/Downloads/OpenROAD-341650e72dad0dc8571822ff8c5d9c5e365327f7.tar.gz
 
     cmakeFlags =
       (cmakeFlagsCommon false)
